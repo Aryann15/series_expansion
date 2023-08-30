0b66ad825a214f6d21b23e1320ed58e2134bda4d
Aryann15 <rajlysm15@gmail.com>
2023-08-30 08:33:00 +0530

    less deviation due to big nmax

--- Taylor_Exp_for_exponentialfn.py
+++ Taylor_Exp_for_exponentialfn.py
@@ -7,3 +7,19 @@ def expTaylor(x,x0, nmax):
             t = t+ np.exp(x0) * (x-x0)**n / np.math.factorial(n)
         return t
 
+expTaylor(2,0,10) #7.388994708994708
+
+print( np.exp(2)) #7.38905609893065
+
+plt.xlabel('x')
+plt.ylabel('y')
+plt.ylim([-5,100])
+
+x_list= np.linspace(-5,5,101)
+plt.scatter(x_list, np.exp(x_list))
+
+nmax= 10
+plt.plot(x_list, expTaylor(x_list,0,nmax), 'blue')
+plt.show()
+
+
