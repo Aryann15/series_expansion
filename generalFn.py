f72dd541701c065acaca78309f934c69d3c41a53
Aryann15 <rajlysm15@gmail.com>
2023-08-30 12:27:03 +0530

    derivative for n higher order

--- generalFn.py
+++ generalFn.py
@@ -8,3 +8,11 @@ def func(x):
     return 2*np.sin(x)**2 + x
 
 print (derivative(func, 1, 0.0001))
+
+def nDerivative (f,x,h,n):
+    t=0
+    for k in range (n+1):
+        t =t + (-1)**(k+n) * np.math.factorial(n)/(np.math.factorial(k)*np.math.factorial(n-k)) * f(x+k*h)
+    return t/h**n
+
+print(nDerivative(func,1,0.0001,1))
