6ec846d175951f242b6bd0564cbcc0258dc22ecb
Aryann15 <rajlysm15@gmail.com>
2023-08-31 21:32:37 +0530

    taylor expansion

--- generalFn.py
+++ generalFn.py
@@ -7,7 +7,7 @@ def derivative ( f,x,h):
 def func(x):
     return 2*np.sin(x)**2 + x
 
-print (derivative(func, 1, 0.0001))
+#print (derivative(func, 1, 0.0001))
 
 def nDerivative (f,x,h,n):
     t=0
@@ -15,4 +15,21 @@ def nDerivative (f,x,h,n):
         t =t + (-1)**(k+n) * np.math.factorial(n)/(np.math.factorial(k)*np.math.factorial(n-k)) * f(x+k*h)
     return t/h**n
 
-print(nDerivative(func,1,0.0001,1))
+#print(nDerivative(func,1,0.0001,1))
+
+def taylorExp(f,x,x0,nmax,h):
+    t= 0
+    for n in range(nmax + 1):
+        t= t+ nDerivative(f,x0,h,n) * (x-x0)**n / np.math.factorial(n)
+    return t
+
+plt.xlabel('x')
+plt.ylabel('y')
+plt.ylim([-8,8])
+
+x_list = np.linspace(-5,5,101)
+plt.scatter(x_list, func(x_list))
+plt.plot(x_list,taylorExp(func, x_list,0,25,0.01),'red')
+plt.plot(x_list,taylorExp(func, x_list,2,15,0.05),'green')
+plt.plot(x_list,taylorExp(func, x_list,4,5,0.01),'blue')
+plt.show()
